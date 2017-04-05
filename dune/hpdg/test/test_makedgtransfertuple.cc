0cbe4965c18574ef293aa52cc1dee75b8dd756f3
Lasse Hinrichsen <lh1887@mi.fu-berlin.de>
2017-04-05 12:12:18 +0200

    Add test for dg transfer tuple generator
    This should have been already in d3366394

---
+++ dune/hpdg/test/test_makedgtransfertuple.cc
@@ -0,0 +1,82 @@
+#include <config.h>
+#include <dune/common/test/testsuite.hh>
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/common/indices.hh>
+#include <dune/common/hybridutilities.hh>
+#include <dune/common/unused.hh>
+
+#include <dune/hpdg/transferoperators/makedgtransfertuple.hh>
+
+#include <dune/grid/yaspgrid.hh>
+#include <dune/grid/utility/structuredgridfactory.hh>
+
+using namespace Dune;
+
+TestSuite test_makedgtransfertuple() {
+  TestSuite suite;
+
+  using Grid = YaspGrid<2>;
+  auto grid = StructuredGridFactory<Grid>::createCubeGrid({0,0}, {1.0,1.0}, {{4,4}});
+
+  using namespace Dune::Indices;
+  {
+    // set up some level pairs
+    constexpr auto level_pairs = std::make_tuple(
+        std::make_pair(_2, _1),
+        std::make_pair(_4, _2)
+        );
+
+    // Compute transfer operators
+    auto transfer = HPDG::make_dgTransferTuple(level_pairs, *grid);
+    // Check if the tuple sizes coincide
+    suite.check(Hybrid::size(transfer) == Hybrid::size(level_pairs), "Check if sizes match") << "Transfer tuple has " <<
+    ((Hybrid::size(transfer) > Hybrid::size(level_pairs)) ? "more" : "less") << " elements than there are level pairs" << std::endl;
+  }
+
+  // try if setup with bad level pairs throws
+  {
+    // case 1: pair is not ordered correctly
+    try {
+      // set up some faulty level pairs
+      constexpr auto level_pairs = std::make_tuple(
+          std::make_pair(_1, _2), // pair is ordered in the wrong way
+          std::make_pair(_4, _2)
+          );
+
+      // Compute transfer operators
+      auto transfer = HPDG::make_dgTransferTuple(level_pairs, *grid);
+      DUNE_UNUSED_PARAMETER(transfer);
+      suite.check(false, "Check handling of wrongly ordered pairs") << "Did not throw where it should have!";
+    }
+    catch (...) {
+      suite.check(true);
+    }
+
+    // case 2: coarse and fine values do not match
+    try {
+      // set up some faulty level pairs
+      constexpr auto level_pairs = std::make_tuple(
+          std::make_pair(_2, _1),
+          std::make_pair(_4, _3) // coarse level has to be _2
+          );
+
+      // Compute transfer operators
+      auto transfer = HPDG::make_dgTransferTuple(level_pairs, *grid);
+      DUNE_UNUSED_PARAMETER(transfer);
+      suite.check(false, "Check handling of consecutive pairs where fine and coarse levels do not match") << "Did not throw where it should have!";
+    }
+    catch (...) {
+      suite.check(true);
+    }
+  }
+  
+  return suite;
+}
+
+int main(int argc, char** argv) {
+  MPIHelper::instance(argc, argv);
+
+  TestSuite suite;
+  suite.subTest(test_makedgtransfertuple());
+  return suite.exit();
+}
