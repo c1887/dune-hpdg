f2701c41b1675e831cc7f07263e2a99225a3032c
Lasse Hinrichsen <lh1887@mi.fu-berlin.de>
2017-04-06 15:25:37 +0200

    Move test matrix generation to new file
    It can be used in other tests as well

---
+++ dune/hpdg/test/testobjects.hh
@@ -0,0 +1,68 @@
+#include <dune/fufem/assemblers/dunefunctionsoperatorassembler.hh>
+#include <dune/fufem/assemblers/istlbackend.hh>
+#include <dune/fufem/assemblers/localassemblers/laplaceassembler.hh>
+
+#include <dune/fufem/assemblers/localassemblers/interiorpenaltydgassembler.hh>
+#include <dune/hpdg/functionspacebases/dgqkglbasis.hh>
+
+/** Assemble a stiffness matrix */
+template<int k, class GridType>
+auto stiffnessMatrix(const GridType& grid) {
+
+  using Matrix = Dune::BCRSMatrix<Dune::FieldMatrix<double, (k+1)*(k+1), (k+1)*(k+1)> >;
+  Matrix matrix;
+
+  const double penalty = 2.0*std::pow(k+1,2); // penalty factor
+
+  /* Setup Basis */
+  using Basis = Dune::Functions::DGQkGLBlockBasis<typename GridType::LeafGridView, k>;
+  Basis basis{grid.leafGridView()};
+
+  /* assemble laplace bulk terms and ipdg terms */
+  {
+    using Assembler = Dune::Fufem::DuneFunctionsOperatorAssembler<Basis, Basis>;
+    using FiniteElement = std::decay_t<decltype(basis.localView().tree().finiteElement())>;
+    auto matrixBackend = Dune::Fufem::istlMatrixBackend(matrix);
+    auto patternBuilder = matrixBackend.patternBuilder();
+
+    auto assembler = Assembler{basis, basis};
+
+    assembler.assembleSkeletonPattern(patternBuilder);
+
+    patternBuilder.setupMatrix();
+
+
+    auto vintageIPDGAssembler = InteriorPenaltyDGAssembler<GridType, FiniteElement, FiniteElement>();
+    vintageIPDGAssembler.sigma0=penalty;
+    vintageIPDGAssembler.dirichlet = true;
+    auto localBlockAssembler = [&](const auto& edge, auto& matrixContainer,
+        auto&& insideTrialLocalView, auto&& insideAnsatzLocalView, auto&& outsideTrialLocalView, auto&& outsideAnsatzLocalView)
+    {
+        vintageIPDGAssembler.assembleBlockwise(edge, matrixContainer, insideTrialLocalView.tree().finiteElement(),
+                                               insideAnsatzLocalView.tree().finiteElement(),
+                                               outsideTrialLocalView.tree().finiteElement(),
+                                               outsideAnsatzLocalView.tree().finiteElement());
+    };
+    auto localBoundaryAssembler = [&](const auto& edge, auto& localMatrix, auto&& insideTrialLocalView, auto&& insideAnsatzLocalView)
+    {
+        vintageIPDGAssembler.assemble(edge, localMatrix, insideTrialLocalView.tree().finiteElement(), insideAnsatzLocalView.tree().finiteElement());
+    };
+
+    assembler.assembleSkeletonEntries(matrixBackend, localBlockAssembler, localBoundaryAssembler); // IPDG terms
+
+    auto vintageBulkAssembler = LaplaceAssembler<GridType,FiniteElement, FiniteElement>();
+
+    auto localAssembler = [&](const auto& element, auto& localMatrix, auto&& trialLocalView, auto&& ansatzLocalView){
+      vintageBulkAssembler.assemble(element, localMatrix, trialLocalView.tree().finiteElement(), ansatzLocalView.tree().finiteElement());
+    };
+
+    /* We need to construct the stiffness Matrix into a separate (temporary) matrix object as otherwise the previously assembled entries
+     * would be lost. This temporary matrix will be deleted after we leave the block scope*/
+    auto bulkMatrix = Matrix{};
+    auto bmatrixBackend = Dune::Fufem::istlMatrixBackend(bulkMatrix);
+    assembler.assembleBulk(bmatrixBackend, localAssembler);
+    matrix+=bulkMatrix;
+  }
+
+  return matrix;
+}
