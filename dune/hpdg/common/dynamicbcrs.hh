d4fdc4b6620734ba32077ab0605b0c6a3d0bea62
Lasse Hinrichsen <lh1887@mi.fu-berlin.de>
2017-08-18 12:04:41 +0200

    Improve DynamicBCRS implementation

--- dune/hpdg/common/dynamicbcrs.hh
+++ dune/hpdg/common/dynamicbcrs.hh
@@ -15,11 +15,12 @@ namespace Dune {
 
     template<class K>
     class DynamicBCRSMatrix {
-      using Block = Dune::HPDG::MatrixWindow<K>;
-      using BCRS = Dune::BCRSMatrix<Block>;
+      using BlockType = Dune::HPDG::MatrixWindow<K>;
+      using BCRS = Dune::BCRSMatrix<BlockType>;
 
       public:
 
+      using Matrix = Dune::BCRSMatrix<BlockType>;
       using field_type = K;
 
       DynamicBCRSMatrix(size_t n, size_t m, size_t blockRows=1) :
@@ -33,30 +34,61 @@ namespace Dune {
         m_(0),
         rowMap_(0) {}
 
+      /** Get the managed BCRS matrix in Dune::BCRSMatrix format*/
       BCRS& matrix() {
         return matrix_;
       }
 
+      /** Get the managed BCRS matrix in Dune::BCRSMatrix format*/
       const BCRS& matrix() const {
         return matrix_;
       }
 
+      /** Set the size of the DynamicBCRS matrix.
+       * Note that this will _not_ modify the index set
+       * of the Dune::BCRSMatrix member!
+       */
       void setSize(size_t n, size_t m) {
         n_=n;
         m_=m;
         rowMap_.resize(n_);
       }
 
+      /** Get the number of rows in the i-th block row */
       size_t& blockRows(size_t i) {
         DUNE_ASSERT_BOUNDS(i<n_);
         return rowMap_[i];
       }
 
+      /** Get the number of rows in the i-th block row */
+      const size_t& blockRows(size_t i) const {
+        DUNE_ASSERT_BOUNDS(i<n_);
+        return rowMap_[i];
+      }
+
+      /** Updates the matrix size after the index set was set externally
+       */
       void finishIdx() {
         setSize(matrix_.N(), matrix_.M());
       }
 
+      /** (Re-)allocates memory and sets pointers of the matrix windows. 
+       *
+       * This is needed before actually using the matrix!
+       */
       void update() {
+        allocateMemory();
+        resetBlocks();
+      }
+
+      private:
+
+      /** Allocates memory.
+       *
+       * If memory has been allocated before, 
+       * it will be released and new memory will be allocated!
+       */
+      void allocateMemory() {
         auto size = calculateSize();
         if (data_ != nullptr) {
           data_.reset(new field_type[size]);
@@ -64,11 +96,9 @@ namespace Dune {
         else {
           data_ = std::make_unique<field_type[]>(size);
         }
-        resetBlocks();
       }
 
-      private:
-
+      /** Calculate the amount of memory needed*/
       size_t calculateSize() const {
         size_t count = 0;
         for (size_t i = 0; i < n_; i++) {
@@ -80,6 +110,7 @@ namespace Dune {
         return count;
       }
 
+      /** Sets the pointers in the matrix windows, aka. initializes the blocks */
       void resetBlocks() {
         auto currentPtr = data_.get();
         for (size_t i = 0; i < n_; i++) {
