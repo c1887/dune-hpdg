75559a57ba000dc0ff40f67173bdd11a72cb0467
Lasse Hinrichsen <lh1887@mi.fu-berlin.de>
2018-09-13 15:55:47 +0200

    Use matrix creator in IPDG projected Jacobi

--- dune/hpdg/matrix-free/localoperators/ipdgblockprojectedjacobi.hh
+++ dune/hpdg/matrix-free/localoperators/ipdgblockprojectedjacobi.hh
@@ -11,6 +11,7 @@
 #include <dune/istl/matrix.hh>
 
 #include "localoperator.hh"
+#include "ipdgdiagonalblock.hh"
 
 namespace Dune {
 namespace Fufem {
@@ -37,7 +38,8 @@ namespace MatrixFree {
         penalty_(penalty),
         dirichlet_(dirichlet),
         localView_(basis_.localView()),
-        localSolver_(std::forward<LS>(localSolver)) {}
+        localSolver_(std::forward<LS>(localSolver)),
+        matrixCreator_(basis_, penalty, dirichlet) {}
 
       void bind(const typename Base::Entity& e)
       {
@@ -48,6 +50,8 @@ namespace MatrixFree {
           e=0;
         localMatrix_.setSize(localVector_.size(), localVector_.size());
         localMatrix_ = 0.0;
+
+        matrixCreator_.bind(e);
       }
 
       // compute matrix diagonal block
@@ -56,98 +60,7 @@ namespace MatrixFree {
 
         const auto& fe = localView_.tree().finiteElement();
 
-        // bulk term: standard laplace:
-        {
-          auto laplace_localAssembler = LaplaceAssembler<typename Basis::GridView::Grid, FE, FE>();
-          laplace_localAssembler.assemble(localView_.element(), localMatrix_, fe, fe);
-        }
-        // now the jump terms involving only the inner basis functions:
-        for (const auto& edge: intersections(basis_.gridView(), localView_.element())) {
-          double avg_factor = 0.5;
-          //double avg_factor = 1.0;
-          if (edge.boundary()) {
-            if(!dirichlet_)
-              continue;
-            else
-              avg_factor = 1.0; // We "average" only along the inner function as there is no outer function for a boundary edge
-          }
-
-          // Even though we do not use the outer edge, we still have to find modify the
-          // penalty parameter accordingly to the higher degree to reassemble the same matrix
-          // blocks as for the actual assembled matrix:
-          auto order = fe.localBasis().order();
-          if (edge.neighbor()) {
-            auto lv = localView_;
-            lv.bind(edge.outside());
-            order = std::max(order, lv.tree().finiteElement().localBasis().order());
-          }
-          auto penalty = penalty_ * std::pow(order, 2.0);
-
-          using FVdimworld = FieldVector<Field, GV::Grid::dimensionworld>;
-
-          using JacobianType = typename FE::Traits::LocalBasisType::Traits::JacobianType;
-          using RangeType = typename FE::Traits::LocalBasisType::Traits::RangeType;
-
-          // get geometry and store it
-          const auto edgeGeometry = edge.geometry();
-          const auto insideGeometry = edge.inside().geometry();
-
-          const auto edgeLength = edgeGeometry.volume();
-
-          // get quadrature rule
-          QuadratureRuleKey tFEquad(edge.type(), order);
-          QuadratureRuleKey quadKey = tFEquad.derivative().square();
-
-          const auto& quad = QuadratureRuleCache<double, dim-1>::rule(quadKey);
-
-          // store gradients
-          std::vector<JacobianType> insideReferenceGradients(fe.localBasis().size());
-          std::vector<FVdimworld> insideGradients(fe.localBasis().size());
-
-          // store values of shape functions
-          std::vector<RangeType> tFEinsideValues(fe.localBasis().size());
-
-          const auto outerNormal = edge.centerUnitOuterNormal();
-
-          // loop over quadrature points
-          for (size_t pt=0; pt < quad.size(); ++pt)
-          {
-            // get quadrature point
-            const auto& quadPos = quad[pt].position();
-
-            // get transposed inverse of Jacobian of transformation
-            const auto& invJacobian = insideGeometry.jacobianInverseTransposed(edge.geometryInInside().global(quadPos));
-
-            // get integration factor
-            const auto integrationElement = edgeGeometry.integrationElement(quadPos);
-
-            // get gradients of shape functions on both the inside and outside element
-            fe.localBasis().evaluateJacobian(edge.geometryInInside().global(quadPos), insideReferenceGradients);
-
-            // transform gradients
-            for (size_t i=0; i<insideGradients.size(); ++i)
-              invJacobian.mv(insideReferenceGradients[i][0], insideGradients[i]);
-
-            // evaluate basis functions
-            fe.localBasis().evaluateFunction(edge.geometryInInside().global(quadPos), tFEinsideValues);
-
-            // compute matrix entries
-            auto z = quad[pt].weight() * integrationElement;
-
-            // Basis functions from inside as test functions
-            for (size_t i=0; i<fe.localBasis().size(); ++i)
-            {
-              // Basis functions from inside as ansatz functions
-              for (size_t j=0; j<fe.localBasis().size(); ++j)
-              {
-                auto zij = -avg_factor*z*tFEinsideValues[i]*(insideGradients[j]*outerNormal)
-                  - avg_factor*z*tFEinsideValues[j]*(insideGradients[i]*outerNormal) // TODO: Just SIPG by now
-                  + penalty*z/edgeLength*tFEinsideValues[i]*tFEinsideValues[j];
-                  localMatrix_[i][j]+=zij;
-              }
-            }
-          }
-        }
+        localMatrix_ = matrixCreator_.matrix();
 
         // now that the localMatrix is set up, we can apply the localsolver:
         auto insideCoeffs = std::vector<Field>(fe.localBasis().size());
@@ -195,6 +108,7 @@ namespace MatrixFree {
       LV localView_;
       std::vector<typename V::field_type> localVector_; // contiguous memory buffer
       LocalSolver localSolver_;
+      HPDG::IPDGDiagonalBlock<V, GV, Basis> matrixCreator_; // Creates the diagonal blocks but ONLY for Gauss-Lobatto Qk bases, so be careful!
       LocalMatrix localMatrix_;
       V lower_;
       V upper_;
---
+++ dune/hpdg/matrix-free/localoperators/ipdgdiagonalblock.hh
@@ -0,0 +1,416 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2
+#ifndef DUNE_FUFEM_MATRIX_FREE_LOCAL_IPDG_BLOCK_JACOBI_HH
+#define DUNE_FUFEM_MATRIX_FREE_LOCAL_IPDG_BLOCK_JACOBI_HH
+#include <dune/common/fmatrix.hh>
+
+#include <dune/istl/matrix.hh>
+
+#include <dune/fufem/assemblers/istlbackend.hh>
+#include <dune/fufem/quadraturerules/quadraturerulecache.hh>
+
+#include <dune/istl/matrix.hh>
+
+/** This is a factory for the diagonal block of an IPDG Laplace discretization.
+ * After binding to an element via bind(), you can get the corresponding
+ * diagonal matrix block of the stiffness matrix via matrix().
+ *
+ * This is particulary useful for block Jacobi algorithms.
+ */
+namespace Dune {
+namespace HPDG {
+
+  template<class V, class GV, class Basis>
+  class IPDGDiagonalBlock {
+    static constexpr int dim = GV::dimension;
+    using LV = typename Basis::LocalView;
+    using Field = typename V::field_type;
+    using FV = Dune::FieldVector<Field, dim>;
+    using LocalMatrix = Dune::Matrix<Dune::FieldMatrix<Field, 1,1>>; // TODO: This is not necessarily 1x1
+
+    static_assert(dim==2, "Sumfactorized IPDG only for dim=2 currently");
+
+    enum class DGType {SIPG = -1, IIPG = 0, NIPG = 1};
+
+    public:
+
+      IPDGDiagonalBlock(const Basis& b, double penalty=2.0, bool dirichlet=false) :
+        basis_(b),
+        penalty_(penalty),
+        dirichlet_(dirichlet),
+        localView_(basis_.localView()) {}
+
+      template<class Entity>
+      void bind(const Entity& e)
+      {
+        localView_.bind(e);
+
+        localMatrix_.setSize(localView_.size(), localView_.size());
+        localMatrix_ = 0.0;
+
+        // get order:
+        localDegree_ = basis_.preBasis().degree(e);
+
+        // check if order is already in cache:
+        {
+          auto it = cache_.find(localDegree_);
+          if (it != cache_.end()) {
+            matrixPair_ = &(it->second);
+            rule_ = &(rules_[localDegree_]);
+            return;
+          }
+        }
+
+        int order = 2*localDegree_ - 1;
+        // get quadrature rule:
+        auto gauss_lobatto = Dune::QuadratureRules<typename GV::Grid::ctype,1>::rule(Dune::GeometryType::cube, order, Dune::QuadratureType::GaussLobatto); // these are the GL lagrange nodes
+        rules_[localDegree_] = Dune::QuadratureRules<typename GV::Grid::ctype,1>::rule(Dune::GeometryType::cube, order+1, Dune::QuadratureType::GaussLobatto); // TODO Welche Ordnung braucht man wirklich?
+
+        rule_ = &(rules_[localDegree_]);
+
+        // sort quad points (they're also ordered for the basis)
+        std::sort(gauss_lobatto.begin(), gauss_lobatto.end(), [](auto&& a, auto&& b) {
+          return a.position() < b.position(); });
+
+        std::sort(rule_->begin(), rule_->end(), [](auto&& a, auto&& b) {
+          return a.position() < b.position(); });
+
+        // save all derivatives of 1-d basis functions at the quad points, i.e.
+        // matrixPair_ij = l_i' (xi_j)
+        LocalMatrix lp(localDegree_+1, rule_->size());
+        // same for the function values
+        LocalMatrix l(localDegree_+1, rule_->size());
+        for (std::size_t i = 0; i < lp.N(); i++) {
+          for (std::size_t j = 0; j < lp.M(); j++) {
+            lp[i][j]=lagrangePrime((*rule_)[j].position(),i, gauss_lobatto);
+            l[i][j]=lagrange((*rule_)[j].position(),i, gauss_lobatto);
+          }
+        }
+        cache_[localDegree_]=std::array<LocalMatrix, 2>{{std::move(l), std::move(lp)}};
+        matrixPair_ = &(cache_[localDegree_]);
+
+      }
+
+      /** Get the matrix for the bound element */
+      const auto& matrix() {
+        compute();
+        return localMatrix_;
+      }
+
+    private:
+      // compute matrix diagonal block
+      // and solve block for input part
+      void compute() {
+
+        // bulk term: standard laplace:
+        {
+          computeBulk();
+          computeFace();
+        }
+
+      }
+
+      void computeFace() {
+        const auto& fe = localView_.tree().finiteElement();
+        // now the jump terms involving only the inner basis functions:
+        for (const auto& edge: intersections(basis_.gridView(), localView_.element())) {
+          double avg_factor = 0.5;
+          if (edge.boundary()) {
+            if(!dirichlet_)
+              continue;
+            else
+              avg_factor = 1.0; // We "average" only along the inner function as there is no outer function for a boundary edge
+          }
+
+          auto order = localDegree_;
+
+          // if the edge is nonconforming, we can not use the precalculated values and need to re-evaluate
+          decltype(nonConformingMatrices(edge)) nc_matrices;
+          auto* edgeMatrices = matrixPair_;
+
+          // set the rule if needed:
+          if (edge.neighbor()) {
+
+            // set order
+            auto outerDegree =basis_.preBasis().degree(edge.outside());
+            order = std::max(localDegree_, outerDegree);
+
+            if (outerDegree > localDegree_) {
+              if(rules_.find(outerDegree) == rules_.end())
+                rules_[outerDegree] = Dune::QuadratureRules<typename GV::Grid::ctype,1>::rule(Dune::GeometryType::cube, 2*outerDegree, Dune::QuadratureType::GaussLobatto); // TODO Welche Ordnung braucht man wirklich?
+              rule_ = &(rules_[outerDegree]);
+            }
+
+            if (!edge.conforming() or outerDegree > localDegree_) {
+              nc_matrices = nonConformingMatrices(edge);
+              edgeMatrices = nc_matrices.get();
+            }
+          }
+
+
+
+          // Even though we do not use the outer edge, we still have to find modify the
+          // penalty parameter accordingly to the higher degree to reassemble the same matrix
+          // blocks as for the actual assembled matrix:
+          auto penalty = penalty_ * std::pow(order, 2.0);
+
+          using FVdimworld = FieldVector<Field, GV::Grid::dimensionworld>;
+
+          // get geometry and store it
+          const auto edgeGeometry = edge.geometry();
+          const auto insideGeometry = edge.inside().geometry();
+
+          const auto edgeLength = edgeGeometry.volume();
+
+          // store gradients
+          std::vector<FVdimworld> insideGradients(fe.localBasis().size());
+          std::vector<Dune::FieldVector<double, 1>> insideValues(fe.localBasis().size());
+
+          const auto outerNormal = edge.centerUnitOuterNormal();
+
+          // loop over quadrature points
+          for (size_t pt=0; pt < rule_->size(); ++pt)
+          {
+            // get quadrature point
+            const auto& quadPos = (*rule_)[pt].position();
+
+            // get transposed inverse of Jacobian of transformation
+            const auto& invJacobian = insideGeometry.jacobianInverseTransposed(edge.geometryInInside().global(quadPos));
+
+            // get integration factor
+            const auto integrationElement = edgeGeometry.integrationElement(quadPos);
+
+            // get gradients of shape functions on both the inside and outside element
+            auto insideReferenceGradients = gradientsOnEdge(pt, *edgeMatrices, edge.indexInInside());
+
+            // transform gradients
+            for (size_t i=0; i<insideGradients.size(); ++i)
+              invJacobian.mv(insideReferenceGradients[i], insideGradients[i]);
+
+            // evaluate basis functions
+            auto insideValues = valuesOnEdge(pt, *edgeMatrices, edge.indexInInside());
+
+            // compute matrix entries
+            auto z = (*rule_)[pt].weight() * integrationElement;
+
+            // Basis functions from inside as test functions
+            for (size_t i=0; i<localView_.size(); ++i)
+            {
+              // Basis functions from inside as ansatz functions
+              for (size_t j=0; j<localView_.size(); ++j)
+              {
+                auto zij = -avg_factor*z*insideValues[i]*(insideGradients[j]*outerNormal)
+                  - avg_factor*z*insideValues[j]*(insideGradients[i]*outerNormal) // TODO: Just SIPG by now
+                  + penalty*z/edgeLength*insideValues[i]*insideValues[j];
+                  localMatrix_[i][j]+=zij;
+              }
+            }
+          }
+          rule_ = &(rules_[localDegree_]);
+        }
+      }
+
+      void computeBulk() {
+        auto geometry = localView_.element().geometry();
+
+        auto size = (localDegree_+1)*(localDegree_+1);
+
+        // store gradients of shape functions and base functions
+        using Gradient = FieldVector<double, dim>;
+        std::vector<Gradient> gradients(size);
+
+        // loop over quadrature points
+        for (size_t q0=0; q0 < rule_->size(); ++q0) {
+          FV quadPos = {{(*rule_)[q0].position(), 0}};
+          for (size_t q1=0; q1 < rule_->size(); ++q1)
+          {
+            // get quadrature point
+            quadPos[1]=(*rule_)[q1].position();
+
+            // get transposed inverse of Jacobian of transformation
+            const auto& invJacobian = geometry.jacobianInverseTransposed(quadPos);
+
+            // get integration factor
+            const auto integrationElement = geometry.integrationElement(quadPos);
+
+            // get gradients of shape functions
+            auto referenceGradients = computeGradients(q0, q1);
+
+            // transform gradients
+            for (size_t i=0; i<gradients.size(); ++i)
+              invJacobian.mv(referenceGradients[i], gradients[i]);
+
+            // compute matrix entries
+            auto z = (*rule_)[q0].weight() * (*rule_)[q1].weight() * integrationElement;
+            for (size_t i=0; i<localMatrix_.N(); ++i)
+            {
+              for (size_t j=i+1; j<localMatrix_.M(); ++j)
+              {
+                double zij = (gradients[i] * gradients[j]) * z;
+                localMatrix_[i][j]+=zij;
+                localMatrix_[j][i]+=zij;
+              }
+              localMatrix_[i][i]+= (gradients[i] * gradients[i]) * z;
+            }
+          }
+        }
+      }
+
+      auto computeGradients(size_t q0, size_t q1) const {
+
+        using Gradient = FieldVector<double, dim>;
+        std::vector<Gradient> referenceGradients(localView_.size());
+
+        for(size_t i = 0; i < localDegree_+1; i++) {
+          for(size_t j = 0; j < localDegree_+1; j++) {
+            referenceGradients[flatIndex(i,j, localDegree_)] = {{(*matrixPair_)[1][i][q0]*(*matrixPair_)[0][j][q1], (*matrixPair_)[0][i][q0]*(*matrixPair_)[1][j][q1]}};
+          }
+        }
+
+        return referenceGradients;
+      }
+
+      template<class MP>
+      auto valuesOnEdge(size_t quad_nr, const MP& edgeMatrices, int  edgeNumber) const {
+
+        using Value = FieldVector<double, 1>;
+        std::vector<Value> vals(localView_.size());
+
+        auto lastIdx = (*matrixPair_)[0].M()-1;
+        for(size_t i = 0; i < localDegree_+1; i++) {
+          for(size_t j = 0; j < localDegree_+1; j++) {
+            switch(edgeNumber) {
+              case 0:
+                vals[flatIndex(i,j, localDegree_)] = {{(*matrixPair_)[0][i][0]*edgeMatrices[0][j][quad_nr]}};
+                break;
+              case 1:
+                vals[flatIndex(i,j, localDegree_)] = {{(*matrixPair_)[0][i][lastIdx]*edgeMatrices[0][j][quad_nr]}};
+                break;
+              case 2:
+                vals[flatIndex(i,j, localDegree_)] = {{edgeMatrices[0][i][quad_nr]*(*matrixPair_)[0][j][0]}};
+                break;
+              case 3:
+                vals[flatIndex(i,j, localDegree_)] = {{edgeMatrices[0][i][quad_nr]*(*matrixPair_)[0][j][lastIdx]}};
+                break;
+              default:
+                DUNE_THROW(Dune::Exception, "Illegal edge number provided");
+            }
+          }
+        }
+
+        return vals;
+      }
+
+      template<class MP>
+      auto gradientsOnEdge(size_t quad_nr, const MP& edgeMatrices, int edgeNumber) const {
+        using Gradient = FieldVector<double, dim>;
+        std::vector<Gradient> referenceGradients(localView_.size());
+
+        auto lastIdx = (*matrixPair_)[0].M()-1;
+        for (size_t i = 0; i< localDegree_ +1; i++) {
+          for(size_t j = 0; j < localDegree_+1; j++) {
+            switch(edgeNumber) {
+              case 0:
+                referenceGradients[flatIndex(i,j, localDegree_)] = {{(*matrixPair_)[1][i][0]*edgeMatrices[0][j][quad_nr],  (*matrixPair_)[0][i][0]*edgeMatrices[1][j][quad_nr]}};
+                break;
+              case 1:
+                referenceGradients[flatIndex(i,j, localDegree_)] = {{(*matrixPair_)[1][i][lastIdx]*edgeMatrices[0][j][quad_nr], (*matrixPair_)[0][i][lastIdx]*edgeMatrices[1][j][quad_nr]}};
+                break;
+              case 2:
+                referenceGradients[flatIndex(i,j, localDegree_)] = {{edgeMatrices[1][i][quad_nr]*(*matrixPair_)[0][j][0], edgeMatrices[0][i][quad_nr]*(*matrixPair_)[1][j][0]}};
+                break;
+              case 3:
+                referenceGradients[flatIndex(i,j, localDegree_)] = {{edgeMatrices[1][i][quad_nr]*(*matrixPair_)[0][j][lastIdx], edgeMatrices[0][i][quad_nr]*(*matrixPair_)[1][j][lastIdx]}};
+                break;
+              default:
+                DUNE_THROW(Dune::Exception, "Illegal edge number provided");
+            }
+          }
+        }
+        return referenceGradients;
+      }
+
+      template<class IS>
+      auto nonConformingMatrices(const IS& is)
+      {
+        // basis nodes:
+        auto gl_i = Dune::QuadratureRules<typename GV::Grid::ctype,1>::rule(Dune::GeometryType::cube, 2*localDegree_-1, Dune::QuadratureType::GaussLobatto); // these are the GL lagrange nodes
+        assert(gl_i.size() == localDegree_+1);
+
+        const auto& rule = *rule_;
+
+        // sort node points (they're also ordered for the basis)
+        std::sort(gl_i.begin(), gl_i.end(), [](auto&& a, auto&& b) {
+          return a.position() < b.position(); });
+
+        // compute inner and outer matrix pair for the noncorming edge.
+        auto inner_quad = std::vector<typename GV::Grid::ctype>(rule.size());
+        for (size_t i = 0; i < rule.size(); i++) {
+          inner_quad[i] = is.geometryInInside().global(rule[i].position())[is.indexInInside() < 2]; // we only take the coordinate we need, which will be the first for edge number 2 and 3, and the second for 0 and 1
+        }
+        auto ret = std::make_unique<std::array<LocalMatrix, 2>>();
+
+        auto& innerPair = (*ret);
+
+        // resize:
+        innerPair[0].setSize(gl_i.size(), inner_quad.size());
+        innerPair[1].setSize(gl_i.size(), inner_quad.size());
+
+        // fill cache
+        for (std::size_t j = 0; j < rule.size(); j++) {
+          for (std::size_t i = 0; i < gl_i.size(); i++) {
+            innerPair[1][i][j]=lagrangePrime(inner_quad[j],i, gl_i);
+            innerPair[0][i][j]=lagrange(inner_quad[j],i, gl_i);
+          }
+        }
+        return ret;
+      }
+
+      auto flatIndex(unsigned int i0, unsigned int i1, int k) const {
+        return i0 + i1*(k+1);
+      }
+
+      template<class X, class Q>
+      inline double lagrangePrime(const X& x, size_t i, const Q& quad) const {
+        double result = 0.;
+
+        for (size_t j=0; j<quad.size(); j++)
+          if (j!=i)
+          {
+            double prod= 1.0/(quad[i].position()-quad[j].position());
+            for (size_t l=0; l<quad.size(); l++)
+              if (l!=i && l!=j)
+                prod *= (x-quad[l].position())/(quad[i].position()-quad[l].position());
+            result += prod;
+          }
+        return result;
+      }
+
+      template<class X, class Q>
+      inline double lagrange(const X& x, size_t i, const Q& quad) const {
+        double result = 1.;
+
+        auto xi= quad[i].position();
+
+        for (size_t j=0; j<quad.size(); j++)
+          if (j!=i)
+          {
+            result*= (x-quad[j].position())/(xi-quad[j].position());
+          }
+        return result;
+      }
+
+      const Basis& basis_;
+      double penalty_;
+      bool dirichlet_;
+      LV localView_;
+      std::map<size_t, std::array<LocalMatrix, 2>> cache_; // contains all lagrange Polynomials at all quadrature points and all derivatives of said polynomials at all quad points
+      std::map<size_t, Dune::QuadratureRule<typename GV::Grid::ctype, 1>> rules_;
+      const typename decltype(cache_)::mapped_type* matrixPair_; // Current matrix pair
+      Dune::QuadratureRule<typename GV::Grid::ctype,1>* rule_;
+      int localDegree_;
+      LocalMatrix localMatrix_;
+  };
+}
+}
+#endif
